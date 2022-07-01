ee9d449104c8cee2211751cd6aa71223df818d71
emil-aliyev <20a.emil01@gmail.com>
2022-07-01 18:33:33 +0400

    "chat initial commit"

---
+++ api/channels.js
@@ -0,0 +1,52 @@
+export const channels = [
+        {name: 'Design',
+        icon: "&#x270F;",
+        members : ['1', '2', '3'],
+        messages : [{
+                text: 'Hello',
+                sender_id : '1',
+                timestamp: '21:18:53'
+            }, 
+            {
+                text: 'Hey',
+                sender_id: '2',
+                timestamp: '21:18:54'
+            },
+            {
+                text: 'Heyo guuuys',
+                sender_id: '3',
+                timestamp: '21:18:56'
+            }]},
+        {
+        name: 'Dev-channel',
+        icon: "&#x1F680;",
+        members : ['2', '4', '1'],
+        messages : [{
+                text: 'Hello',
+                sender_id : '1',
+                timestamp: '21:18:53'
+            }, 
+            {
+                text: 'Hey',
+                sender_id: '4',
+                timestamp: '21:18:54'
+            },
+            {
+                text: 'Heyo guuuys',
+                sender_id: '2',
+                timestamp: '21:18:56'
+            }]},
+        {
+        name: 'Finance',
+        icon: "&#127974;",
+        members : ['2', '1'],
+        messages : [{
+                text: 'Hello',
+                sender_id : '1',
+                timestamp: '21:18:53'
+            }, 
+            {
+                text: 'Hey',
+                sender_id: '2',
+                timestamp: '21:18:54'
+            }]}]
---
+++ api/users.js
@@ -0,0 +1,22 @@
+export const users = {
+    '1' : {
+        name: 'John Snow',
+        permission: 'user',
+        icon: 'none'
+    },
+    '2' : {
+        name: 'Daenerys Targaryen',
+        permission: 'admin',
+        icon: 'none'
+    },
+    '3' : {
+        name : 'Arya Start',
+        permission: 'user',
+        icon : 'none'
+    },
+    '4' : {
+        name : 'Cercei Lanister',
+        permission: 'admin',
+        icon : 'none'
+    }
+}
